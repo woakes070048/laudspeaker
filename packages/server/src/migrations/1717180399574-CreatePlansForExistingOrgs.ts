07626b6d58245fefb217ebfccaf1c18f24d1ce9b
Moustafa Eid <eidmoustafa@gmail.com>
2024-05-31 18:08:21 -0700

    update migrations to fix staging (#465)
    * update migrations to fix staging
    
    * fix

--- packages/server/src/migrations/1717180399574-CreatePlansForExistingOrgs.ts
+++ packages/server/src/migrations/1717180399574-CreatePlansForExistingOrgs.ts
@@ -17,6 +17,10 @@ export class CreatePlansForExistingOrgs1717180399574 implements MigrationInterfa
       }
 
       await queryRunner.query(`DELETE FROM organization_plan where id = '${existing_plan_id}'`);
+
+      await queryRunner.query(`ALTER TABLE public.organization ALTER COLUMN "planId" SET NOT NULL`);
+      await queryRunner.query(`ALTER TABLE ONLY public.organization ADD CONSTRAINT "UQ_9838b0567dc51c15e3679b2a6e3" UNIQUE ("planId")`);
+      await queryRunner.query(`ALTER TABLE "organization" ADD CONSTRAINT "FK_9838b0567dc51c15e3679b2a6e3" FOREIGN KEY ("planId") REFERENCES "organization_plan"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
