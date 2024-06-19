ba0636eac640ef26aa10786518a3dacfdfdacc70
abheek9 <abheek94@gmail.com>
2024-06-18 23:32:25 -0400

    UI/move tooltip (#494)
    * properly saving opens
    
    * showing full number
    
    * added env to not track num of message  sends
    
    * changed position of tooltip

--- packages/client/src/pages/FlowViewerv2/SidePanel/viewers/MessageViewer.tsx
+++ packages/client/src/pages/FlowViewerv2/SidePanel/viewers/MessageViewer.tsx
@@ -80,19 +80,19 @@ const MessageViewer: FC<SidePanelComponentProps<MessageNodeData>> = ({
                     )
                   }
                 >
-                  <div className="font-roboto text-[14px] leading-[22px] text-[#6B7280]">
-                    {stat.name}
-                  </div>
-                  <div className="font-roboto text-[24px] leading-[28px]">
-                    <Tooltip
+                  <Tooltip
                       placement="top-start"
                       className="bg-[#111827] rounded-none p-[5px] text-white"
                       content={`${nodeData.stats?.[stat.key] || 0}`}
-                    >
+                  >
+                  <div className="font-roboto text-[14px] leading-[22px] text-[#6B7280]">
+                    {stat.name}
+                  </div>
+                  </Tooltip>
+                  <div className="font-roboto text-[24px] leading-[28px]">
                       <div className="relative">
                         <div className="w-[8px] h-[8px] bg-transparent rounded-[100%]" />
                       </div>
-                    </Tooltip>
                     {stat.renderLabel(nodeData.stats?.[stat.key] || 0)}
                   </div>
                 </div>
